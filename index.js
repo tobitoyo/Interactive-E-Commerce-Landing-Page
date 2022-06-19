3e769250a2376e9c3453eb3bc687e0a73336dd78
Tobi Toyo <toyocodes@gmail.com>
2022-06-19 13:20:39 -0700

    Mobile interface is 65% done

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let column = document.getElementById("nav-column");
+
+function openNavColumn() {
+    column.style.display = "block"
+}
+
+function closeNavColumn() {
+    column.style.display = "none"
+}
